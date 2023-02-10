b359f35334de3f1b5f0746cd3b01c3f92cba6d8a
SherbakovSAF <sherbakovsaf@gmail.com>
2023-02-10 19:34:17 +0300

    Добавление эллемента по введёному значению

--- src/components/FirstComponent.vue
+++ src/components/FirstComponent.vue
@@ -14,7 +14,7 @@
                               <div class="mt-1 relative rounded-md shadow-md">
                                    <input type="text" name="wallet" id="wallet"
                                         class="block w-full pr-10 border-gray-300 text-gray-900 focus:outline-none focus:ring-gray-500 focus:border-gray-500 sm:text-sm rounded-md"
-                                        placeholder="Например DOGE" />
+                                        placeholder="Например DOGE" v-model="inputTicket"/>
                               </div>
                               <div class="flex bg-white shadow-md p-1 rounded-md shadow-md flex-wrap">
                                    <span
@@ -38,6 +38,7 @@
                          </div>
                     </div>
                     <button type="button"
+                         v-on:click="addTicket"
                          class="my-4 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">
                          <!-- Heroicon name: solid/mail -->
                          <svg class="-ml-0.5 mr-2 h-6 w-6" xmlns="http://www.w3.org/2000/svg" width="30" height="30"
@@ -53,13 +54,15 @@
                <hr class="w-full border-t border-gray-600 my-4" />
                <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
                     <div
+                         v-for="tiket in tiketState"
+                         v-bind:key="tiket.id"
                          class="bg-white overflow-hidden shadow rounded-lg border-purple-800 border-solid cursor-pointer">
                          <div class="px-4 py-5 sm:p-6 text-center">
                               <dt class="text-sm font-medium text-gray-500 truncate">
-                                   WTF - USD
+                                   {{ tiket.name }}
                               </dt>
                               <dd class="mt-1 text-3xl font-semibold text-gray-900">
-                                   1.11
+                                   {{ tiket.price }}
                               </dd>
                          </div>
                          <div class="w-full border-t border-gray-200"></div>
@@ -103,7 +106,27 @@
 <script>
 export default {
      name: 'App',
-     components: {
+     data() {
+          return {
+               inputTicket: "",
+               tiketState: [
+                    {
+                    id: 0,
+                    name: `BTC - 2`,
+                    price: "111"
+                    },
+               ]
+          }
+     },
+     methods: {
+          addTicket(){
+               let newTicket = {
+                    id: this.tiketState.length,
+                    name: `BTC - ${this.inputTicket}`,
+                    price: "111"
+               }
+               this.tiketState.push(newTicket)
+          },
      }
 }
 </script>
