f46f089c6c9139d4af802c72c53c355e0b2d5e84
SherbakovSAF <sherbakovsaf@gmail.com>
2023-02-11 14:45:01 +0300

    Добавил удаление карточки с валютой

--- src/components/FirstComponent.vue
+++ src/components/FirstComponent.vue
@@ -12,11 +12,12 @@
                          <div class="max-w-xs">
                               <label for="wallet" class="block text-sm font-medium text-gray-700">Тикер</label>
                               <div class="mt-1 relative rounded-md shadow-md">
-                                   <input type="text" name="wallet" id="wallet"
+                                   <input
+                                        v-model="inputTicket"
+                                        v-on:focus="removeAlertMessage()" 
+                                        type="text" name="wallet" id="wallet"
                                         class="block w-full pr-10 border-gray-300 text-gray-900 focus:outline-none focus:ring-gray-500 focus:border-gray-500 sm:text-sm rounded-md"
                                         placeholder="Например DOGE"
-                                        v-model="inputTicket"
-                                        v-on:focus="removeAlertMessage()"
                                         />
                               </div>
                               <div class="flex bg-white shadow-md p-1 rounded-md shadow-md flex-wrap">
@@ -40,7 +41,9 @@
                               <div class="text-sm text-red-600">{{ alertMessage }}</div>
                          </div>
                     </div>
-                    <button type="button" v-on:click="addTicket()"
+                    <button 
+                         type="button"
+                         v-on:click="addTicket()"
                          class="my-4 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">
                          <!-- Heroicon name: solid/mail -->
                          <svg class="-ml-0.5 mr-2 h-6 w-6" xmlns="http://www.w3.org/2000/svg" width="30" height="30"
@@ -67,13 +70,14 @@
                               </div>
                               <div class="w-full border-t border-gray-200"></div>
                               <button
+                                   v-on:click="removeTicket(tiket.id)"
                                    class="flex items-center justify-center font-medium w-full bg-gray-100 px-4 py-4 sm:px-6 text-md text-gray-500 hover:text-gray-600 hover:bg-gray-200 hover:opacity-20 transition-all focus:outline-none">
                                    <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
                                         fill="#718096" aria-hidden="true">
                                         <path fill-rule="evenodd"
                                              d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
                                              clip-rule="evenodd"></path>
-                                   </svg>Удалить
+                                   </svg>Удалить 
                               </button>
                          </div>
                     </dl>
@@ -110,9 +114,7 @@ export default {
      data() {
           return {
                inputTicket: "",
-               tiketState: [
-
-               ],
+               tiketState: [],
                alertMessage: "",
           }
      },
@@ -133,6 +135,9 @@ export default {
           removeAlertMessage(){
                this.alertMessage = ""
           },
+          removeTicket(idTicket){
+               this.tiketState= this.tiketState.filter(e=> e.id !== idTicket)
+          }
      }
 }
 </script>
