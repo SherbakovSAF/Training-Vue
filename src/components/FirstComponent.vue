771927b5309f18a4eba37aceb34d00232c74c71f
SherbakovSAF <sherbakovsaf@gmail.com>
2023-02-11 14:32:27 +0300

    Добавил проверку на пустой ввод
    Сделал удаление блока, если массив с валютами пустой

--- src/components/FirstComponent.vue
+++ src/components/FirstComponent.vue
@@ -14,7 +14,10 @@
                               <div class="mt-1 relative rounded-md shadow-md">
                                    <input type="text" name="wallet" id="wallet"
                                         class="block w-full pr-10 border-gray-300 text-gray-900 focus:outline-none focus:ring-gray-500 focus:border-gray-500 sm:text-sm rounded-md"
-                                        placeholder="Например DOGE" v-model="inputTicket"/>
+                                        placeholder="Например DOGE"
+                                        v-model="inputTicket"
+                                        v-on:focus="removeAlertMessage()"
+                                        />
                               </div>
                               <div class="flex bg-white shadow-md p-1 rounded-md shadow-md flex-wrap">
                                    <span
@@ -34,11 +37,10 @@
                                         CHD
                                    </span>
                               </div>
-                              <div class="text-sm text-red-600">Такой тикер уже добавлен</div>
+                              <div class="text-sm text-red-600">{{ alertMessage }}</div>
                          </div>
                     </div>
-                    <button type="button"
-                         v-on:click="addTicket"
+                    <button type="button" v-on:click="addTicket()"
                          class="my-4 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">
                          <!-- Heroicon name: solid/mail -->
                          <svg class="-ml-0.5 mr-2 h-6 w-6" xmlns="http://www.w3.org/2000/svg" width="30" height="30"
@@ -50,35 +52,33 @@
                          Добавить
                     </button>
                </section>
-
-               <hr class="w-full border-t border-gray-600 my-4" />
-               <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
-                    <div
-                         v-for="tiket in tiketState"
-                         v-bind:key="tiket.id"
-                         class="bg-white overflow-hidden shadow rounded-lg border-purple-800 border-solid cursor-pointer">
-                         <div class="px-4 py-5 sm:p-6 text-center">
-                              <dt class="text-sm font-medium text-gray-500 truncate">
-                                   {{ tiket.name }}
-                              </dt>
-                              <dd class="mt-1 text-3xl font-semibold text-gray-900">
-                                   {{ tiket.price }}
-                              </dd>
+               <template v-if="tiketState.length">
+                    <hr class="w-full border-t border-gray-600 my-4" />
+                    <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
+                         <div v-for="tiket in tiketState" v-bind:key="tiket.id"
+                              class="bg-white overflow-hidden shadow rounded-lg border-purple-800 border-solid cursor-pointer">
+                              <div class="px-4 py-5 sm:p-6 text-center">
+                                   <dt class="text-sm font-medium text-gray-500 truncate">
+                                        {{ tiket.name }}
+                                   </dt>
+                                   <dd class="mt-1 text-3xl font-semibold text-gray-900">
+                                        {{ tiket.price }}
+                                   </dd>
+                              </div>
+                              <div class="w-full border-t border-gray-200"></div>
+                              <button
+                                   class="flex items-center justify-center font-medium w-full bg-gray-100 px-4 py-4 sm:px-6 text-md text-gray-500 hover:text-gray-600 hover:bg-gray-200 hover:opacity-20 transition-all focus:outline-none">
+                                   <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
+                                        fill="#718096" aria-hidden="true">
+                                        <path fill-rule="evenodd"
+                                             d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+                                             clip-rule="evenodd"></path>
+                                   </svg>Удалить
+                              </button>
                          </div>
-                         <div class="w-full border-t border-gray-200"></div>
-                         <button
-                              class="flex items-center justify-center font-medium w-full bg-gray-100 px-4 py-4 sm:px-6 text-md text-gray-500 hover:text-gray-600 hover:bg-gray-200 hover:opacity-20 transition-all focus:outline-none">
-                              <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="#718096"
-                                   aria-hidden="true">
-                                   <path fill-rule="evenodd"
-                                        d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
-                                        clip-rule="evenodd"></path>
-                              </svg>Удалить
-                         </button>
-                    </div>
-               </dl>
-               <hr class="w-full border-t border-gray-600 my-4" />
-               <section class="relative">
+                    </dl>
+                    <hr class="w-full border-t border-gray-600 my-4" />
+                    <!-- <section class="relative">
                     <h3 class="text-lg leading-6 font-medium text-gray-900 my-8">
                          VUE - USD
                     </h3>
@@ -99,7 +99,8 @@
                               </g>
                          </svg>
                     </button>
-               </section>
+               </section> -->
+               </template>
           </div>
      </div>
 </template>
@@ -110,22 +111,27 @@ export default {
           return {
                inputTicket: "",
                tiketState: [
-                    {
-                    id: 0,
-                    name: `BTC - 2`,
-                    price: "111"
-                    },
-               ]
+
+               ],
+               alertMessage: "",
           }
      },
      methods: {
-          addTicket(){
+          addTicket() {
                let newTicket = {
                     id: this.tiketState.length,
                     name: `BTC - ${this.inputTicket}`,
                     price: "111"
                }
-               this.tiketState.push(newTicket)
+               if(this.inputTicket){
+                    this.tiketState.push(newTicket)
+                    this.inputTicket = ""
+               } else {
+                    this.alertMessage = "Введите название"
+               }
+          },
+          removeAlertMessage(){
+               this.alertMessage = ""
           },
      }
 }
