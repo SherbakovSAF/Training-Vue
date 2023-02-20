49da7143db0f8eb183dae283f25b472363208f31
SherbakovSAF <sherbakovsaf@gmail.com>
2023-02-20 23:47:55 +0300

    Добавил некоторые функции в watch. Сделал возможность обнуления графика у тикера

--- src/components/FirstComponent.vue
+++ src/components/FirstComponent.vue
@@ -47,6 +47,7 @@
                          </svg>
                          Добавить
                     </button>
+                    
                </section>
                <hr class="w-full border-t border-gray-600 my-4" />
                <div>
@@ -97,33 +98,36 @@
                                              clip-rule="evenodd"></path>
                                    </svg>Удалить 
                               </button>
-                              
                          </div>
                     </dl>
                     <hr class="w-full border-t border-gray-600 my-4" />
-                    <button v-on:click="addGraph()">Добавить</button>
-                    <button v-on:click="stopInterval()">Остановить</button>
                     <section v-if="checkedTicket" class="relative">
-                         <h3 class="text-lg leading-6 font-medium text-gray-900 my-8">
-                              USD - {{ checkedTicket.name }}
-                         </h3>
-                         <div class="flex items-end border-gray-600 border-b border-l h-64">
-                              <div
-                              v-for="(graph, idx) in normalizedGraph"
-                              v-bind:key="idx"
-                              :style="{height: graph+'%'}"
-                              class="bg-purple-800 border w-10"></div>
-                         </div>
-                         <button v-on:click="checkedTicket = null" type="button" class="absolute top-0 right-0">
-                              
-                              <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
-                                   xmlns:svgjs="http://svgjs.com/svgjs" version="1.1" width="30" height="30" x="0" y="0"
-                                   viewBox="0 0 511.76 511.76" style="enable-background:new 0 0 512 512" xml:space="preserve">
-                                   <g>
-                                        <path d="M436.896,74.869c-99.84-99.819-262.208-99.819-362.048,0c-99.797,99.819-99.797,262.229,0,362.048    c49.92,49.899,115.477,74.837,181.035,74.837s131.093-24.939,181.013-74.837C536.715,337.099,536.715,174.688,436.896,74.869z     M361.461,331.317c8.341,8.341,8.341,21.824,0,30.165c-4.16,4.16-9.621,6.251-15.083,6.251c-5.461,0-10.923-2.091-15.083-6.251    l-75.413-75.435l-75.392,75.413c-4.181,4.16-9.643,6.251-15.083,6.251c-5.461,0-10.923-2.091-15.083-6.251    c-8.341-8.341-8.341-21.845,0-30.165l75.392-75.413l-75.413-75.413c-8.341-8.341-8.341-21.845,0-30.165    c8.32-8.341,21.824-8.341,30.165,0l75.413,75.413l75.413-75.413c8.341-8.341,21.824-8.341,30.165,0    c8.341,8.32,8.341,21.824,0,30.165l-75.413,75.413L361.461,331.317z"
-                                             fill="#718096" data-original="#000000"></path>
-                                   </g>
-                              </svg>
+                                   <h3 class="text-lg leading-6 font-medium text-gray-900 my-8">
+                                        USD - {{ checkedTicket.name }}
+                                   </h3>
+                                   <div class="flex items-end border-gray-600 border-b border-l h-64">
+                                        <div
+                                        v-for="(graph, idx) in normalizedGraph"
+                                        v-bind:key="idx"
+                                        :style="{ height: graph + '%' }"
+                                        class="bg-purple-800 border w-10"></div>
+                                   </div>
+                                   <button v-on:click="checkedTicket = null" type="button" class="absolute top-0 right-0">
+                                        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+                                             xmlns:svgjs="http://svgjs.com/svgjs" version="1.1" width="30" height="30" x="0" y="0"
+                                             viewBox="0 0 511.76 511.76" style="enable-background:new 0 0 512 512" xml:space="preserve">
+                                             <g>
+                                                  <path d="M436.896,74.869c-99.84-99.819-262.208-99.819-362.048,0c-99.797,99.819-99.797,262.229,0,362.048    c49.92,49.899,115.477,74.837,181.035,74.837s131.093-24.939,181.013-74.837C536.715,337.099,536.715,174.688,436.896,74.869z     M361.461,331.317c8.341,8.341,8.341,21.824,0,30.165c-4.16,4.16-9.621,6.251-15.083,6.251c-5.461,0-10.923-2.091-15.083-6.251    l-75.413-75.435l-75.392,75.413c-4.181,4.16-9.643,6.251-15.083,6.251c-5.461,0-10.923-2.091-15.083-6.251    c-8.341-8.341-8.341-21.845,0-30.165l75.392-75.413l-75.413-75.413c-8.341-8.341-8.341-21.845,0-30.165    c8.32-8.341,21.824-8.341,30.165,0l75.413,75.413l75.413-75.413c8.341-8.341,21.824-8.341,30.165,0    c8.341,8.32,8.341,21.824,0,30.165l-75.413,75.413L361.461,331.317z"
+                                                       fill="#718096" data-original="#000000"></path>
+                                             </g>
+                                        </svg>
+                                   </button>
+                         <button 
+                              type="button"
+                              v-on:click="clearTicketGraph()"
+                              class="w-full my-4 items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">
+                              <!-- Heroicon name: solid/mail -->
+                              Очистить график
                          </button>
                     </section>
                </template>
@@ -179,12 +183,8 @@ export default {
           },
      },
      methods: {
-          
-          addGraph(){
-               this.graphValue.push(Math.ceil(Math.random() * 10000))
-          },
-          stopInterval(){
-               clearInterval(this.interval)
+          clearTicketGraph(){
+               this.ticketState.find(e=> e == this.checkedTicket).graphTicket = [50]
           },
           getPrice(newTicketObject) {
                this.interval = setInterval(async () => {
@@ -229,9 +229,10 @@ export default {
           },
           removeTicket(ticket){
                this.ticketState = this.ticketState.filter(e => e.id !== ticket.id)
-               if(this.checkedTicket != null){
-                    if(this.checkedTicket.name === ticket.name)this.checkedTicket = null
+               if(this.checkedTicket === ticket){
+                    this.checkedTicket = null
                }
+               
           },
           checkRepeatName(){
                return this.ticketState.findIndex(e=>e.name.toLowerCase() == this.inputTicket.toLowerCase()) 
@@ -275,13 +276,19 @@ export default {
           this.renderTemplateInput()
      },
      watch: {
+          paginatedTicker(){
+               if(this.paginatedTicker.length === 0 && this.page > 1){
+                    this.page -= 1
+               }
+          },
           filterInput(){
                this.page = 1
                window.history.pushState(null, document.title, `${window.location.pathname}?filter=${this.filterInput}&page=${this.page}`)
           },
           page(){
                window.history.pushState(null, document.title, `${window.location.pathname}?filter=${this.filterInput}&page=${this.page}`)
-          }
+          },
+
      }
      
 }
