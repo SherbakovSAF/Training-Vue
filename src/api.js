42193ca5638cca1f86955eb96e9a49c2ff242de9
SherbakovSAF <sherbakovsaf@gmail.com>
2023-03-13 16:16:09 +0300

    Добавил сохранение данных, привязанных к тикеру

--- src/api.js
+++ src/api.js
@@ -2,24 +2,26 @@ const API_KEY = "8a2f568b4445642de49bff74fc1df1cca20e845613170855ff41b9bdf6edf24
 
 const tickersHandlers = new Map()
 const socket = new WebSocket(`wss://streamer.cryptocompare.com/v2?api_key=${API_KEY}`)
-
 const AGGREGATE_INDEX = '5'
 
-socket.addEventListener('message', e=>{
-    console.log(e)
-    const {TYPE: type, FROMSYMBOL: currency, PRICE: newPrice} = JSON.parse(e.data)
-    if(newPrice === undefined){
-        return
+// Отслеживание события ВебСокета с извлечением данных для замены
+socket.addEventListener("message", (e) => {
+    const {
+        TYPE: type,
+        FROMSYMBOL: currency,
+        PRICE: newPrice,
+    } = JSON.parse(e.data);
+    if (newPrice === undefined) {
+        return;
     }
-    if(type != AGGREGATE_INDEX){
-        return
+    if (type != AGGREGATE_INDEX) {
+        return;
     }
-    
-    const handlers = tickersHandlers.get(currency) ?? []
-    handlers.forEach(fn => fn(newPrice))
-})
+    const handlers = tickersHandlers.get(currency) ?? [];
+    handlers.forEach((fn) => fn(newPrice));
+});
 
-
+// Отправление сообщения для подписки на тикет
 function sendToWebSocket(message){
     const stringifiedMessage = JSON.stringify(message)
     if(socket.readyState === WebSocket.OPEN) {
@@ -32,6 +34,7 @@ function sendToWebSocket(message){
     {once: true})
 }
 
+// Шаблон сообщения для подписки на получение данных из WebSocket'a
 function subscribeToTickerOwnWS(ticker){
     sendToWebSocket({
         "action": "SubAdd",
@@ -39,6 +42,7 @@ function subscribeToTickerOwnWS(ticker){
     })
 }
 
+// Шаблон сообщения для отписки на получение данных из WebSocket'a
 function unsubscribeFromTickerOwnWS(ticker){
     sendToWebSocket({
         "action": "SubRemove",
@@ -46,16 +50,16 @@ function unsubscribeFromTickerOwnWS(ticker){
     })
 }
 
+// Добавление названия тикера из отслеживаемых WebSocketon названий
 export const subscribeToTicker = (ticker, cb) =>{
      const subscribers = tickersHandlers.get(ticker) || []
      tickersHandlers.set(ticker, [...subscribers, cb])
      subscribeToTickerOwnWS(ticker)
 }
 
+// Удаления названия тикера из отслеживаемых WebSocketon названий
 export const unsubscribeFromTicker = (ticker) =>{
     tickersHandlers.delete(ticker)
     unsubscribeFromTickerOwnWS(ticker)
 }
 
-
-window.tickersHandlers = tickersHandlers
